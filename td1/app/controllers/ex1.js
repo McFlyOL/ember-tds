75c5c7bf1285b0a37a3ff59c10b6ad2b3386ecf4
Thomas <21820757@etu.unicaen.fr>
2020-01-26 20:43:45 +0100

    add save and clear functions

--- td1/app/controllers/ex1.js
+++ td1/app/controllers/ex1.js
@@ -1,4 +1,15 @@
 import Controller from '@ember/controller';
 
 export default class Ex1Controller extends Controller {  
+    actions={
+        save(note){
+          if(note.get('content')) {
+            note.set('info', 'Votre note est sauvegardée : '+note.get('content'));
+          }
+        },
+        clear(note){
+            note.set('content','');
+            note.set('info','');
+        }
+      }
 }
