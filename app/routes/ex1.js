d3e3a998e8232a64e9e6149708dd567b983ce240
Thomas <21820757@etu.unicaen.fr>
2020-02-19 09:37:37 +0100

    first commit

---
+++ app/routes/ex1.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+import Note from 'td1/utils/note-class';
+
+export default class Ex1Route extends Route {
+    model() {
+        return Note.create();
+      }
+      actions={
+        fermer(){
+            this.transitionTo("");
+        }
+    }
+}
+
