09c7446ca5d22a38c3a2abb9ee2d103332cbe375
Thomas <21820757@etu.unicaen.fr>
2020-03-03 15:10:51 +0100

    add td3

--- td1/app/routes/contact.js
+++ td1/app/routes/contact.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import EmberObject,{computed} from '@ember/object';
 import { A } from '@ember/array';
 
-/*let contacts=EmberObject.extend({
+let contacts=EmberObject.extend({
     datas:A(),
     filtre:A(),
     contacts :computed('filtre', 'datas.@each.isDeleted', function() {
@@ -14,7 +14,7 @@ import { A } from '@ember/array';
     deletedsCount : computed("datas", function(){
         return this.get("deleteds").length;
     })
-});*/
+});
 
 export default Route.extend({
     model() {
